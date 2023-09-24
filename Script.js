4848c3510641a0f1527e794368a551ff78b82aa4
Farhan Ahmed (Frimin) <Farhanrjcw389@gmail.com>
2023-09-24 15:30:49 +0500

    Lecture-1
    How display you text and values

---
+++ Script.js
@@ -0,0 +1,12 @@
+//  How display your text in javascript
+console.log("Hello World!");
+// how to create a funcation then values plus then show text
+function addtwotext(a,b){
+    return a+b
+}
+let c = addtwotext(1,2)
+// this the one condtition 
+console.log("Plus Values iS: " + c)
+
+// the second one 
+console.log(addtwotext(1,5))
